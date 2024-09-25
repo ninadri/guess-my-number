dbef858a85ec3375c5f3ccafed123af4f00c6c0d
ninadri <159666628+ninadri@users.noreply.github.com>
2024-09-25 18:09:58 -0500

    added if statement for number guess

--- script.js
+++ script.js
@@ -7,10 +7,21 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const x = function () {
-  console.log(23);
-};
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low!';
+  }
 });
