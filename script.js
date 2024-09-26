21e1d533af18b129457f96e77b7cab90452c80f1
ninadri <159666628+ninadri@users.noreply.github.com>
2024-09-25 19:22:57 -0500

    added queru selectors when guessed too high or too low

--- script.js
+++ script.js
@@ -9,19 +9,41 @@ console.log(document.querySelector('.guess').value);
 */
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too High!';
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too Low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
