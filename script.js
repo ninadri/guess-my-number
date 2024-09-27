6c1ea1001aedbdf78f9de9bcf3254f9ee48869de
ninadri <159666628+ninadri@users.noreply.github.com>
2024-09-27 18:28:17 -0500

    added if statement for highscore

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,8 +18,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
